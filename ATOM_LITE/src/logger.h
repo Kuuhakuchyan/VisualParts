86790c76d0242fbca66b74659a67e459add619fd
Kuuhakuchyan <xyu2392544578@gmail.com>
2026-05-11 18:54:55 +0800

    chore: remove ATOM_LITE from M5Stick_Weather (now sibling in IoT/)

--- ATOM_LITE/src/logger.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-bool logger_init();
-void logger_write(float temp, float humid);
-void logger_dump();
-bool logger_clear();
-unsigned long logger_get_count();
-unsigned long logger_get_size();
-const char*   logger_get_filename();
--- ATOM_LITE/src/sht30.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-bool sht30_init(int sda = 26, int scl = 32);
-bool sht30_read(float &temp, float &humid);
-bool sht30_is_found();
